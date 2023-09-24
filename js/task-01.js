14e5a89e2caf630babca9fb6f08f1467e152052b
Kerabera13 <kerabera1310@gmail.com>
2023-09-24 20:31:34 +0300

    Task 1 changes

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,12 @@
-// Отримуємо список усіх елементів li.item
 const categoriesList = document.querySelectorAll('ul#categories > li.item');
 
-// Виводимо кількість категорій
-console.log(`Всього категорій: ${categoriesList.length}`);
+console.log(`Number of categories: ${categoriesList.length}`);
 
-// Ітеруємося по кожній категорії
 categoriesList.forEach(category => {
-  // Отримуємо текст заголовку (тегу <h2>) кожної категорії
   const categoryName = category.querySelector('h2').textContent;
   
-  // Отримуємо список усіх підкатегорій (елементів <li>) в даній категорії
   const subcategoriesList = category.querySelectorAll('ul > li');
   
-  // Виводимо текст заголовку та кількість підкатегорій
-  console.log(`Категорія: ${categoryName}`);
-  console.log(`Кількість елементів в категорії "${categoryName}": ${subcategoriesList.length}`);
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: "${categoryName}": ${subcategoriesList.length}`);
 });
